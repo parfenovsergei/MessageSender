0702738b463188a23d00f38d3b1e4c6a33e51fea
Sergei Parfenov <parfenovserezhka@gmail.com>
2023-02-27 13:11:11 +0300

    Scripts for DB

---
+++ SQL/createTables.sql
@@ -0,0 +1,19 @@
+use MessageSender
+
+create table Users
+(
+	Id int primary key identity(1,1),
+	Email nvarchar(100) not null,
+	[Password] char(128) not null,
+	[Role] nvarchar(50) not null
+)
+
+create table [Messages]
+(
+	Id int primary key identity(1,1),
+	MessageTheme nvarchar(100) not null,
+	MessageBody nvarchar(1000) not null,
+	SenderId int references Users (Id) not null,
+	ReceiverId int references Users (Id) not null,
+	SendDate datetime not null
+)
