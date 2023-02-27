9687d005c6027c1e670e0789823c9e2b1006a820
Sergei Parfenov <parfenovserezhka@gmail.com>
2023-02-27 13:20:17 +0300

    Add models and enums

---
+++ MessageSenderAPI/MessageSenderAPI/Domain/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageSenderAPI.Domain.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string MessageTheme { get; set; }
+        public string MessageBody { get; set; }
+        public virtual User User { get; set; }
+        public DateTime SendDate { get; set; }
+    }
+}
