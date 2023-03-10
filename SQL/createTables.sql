1819419b9b7edd58ad4a0ece7e621de87f833e87
Sergei Parfenov <parfenovserezhka@gmail.com>
2023-03-10 15:20:31 +0300

    Change email max length in sql

--- SQL/createTables.sql
+++ SQL/createTables.sql
@@ -2,8 +2,8 @@ use MessageSender
 
 create table Users
 (
-	Id int primary key identity(1,1),
-	Email nvarchar(100) not null,
+	Id int primary key identity(1, 1),
+	Email nvarchar(256) not null,
 	[Password] char(128) not null,
 	Salt char(128) not null,
 	[Role] int not null
@@ -11,7 +11,7 @@ create table Users
 
 create table [Messages]
 (
-	Id int primary key identity(1,1),
+	Id int primary key identity(1, 1),
 	MessageTheme nvarchar(100) not null,
 	MessageBody nvarchar(1000) not null,
 	OwnerId int references Users (Id) not null,
